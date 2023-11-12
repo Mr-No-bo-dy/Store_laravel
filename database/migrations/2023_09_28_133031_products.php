831cb70db1b4f2bbf715c4c7d8cb3c181ef7bcc5
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2023-11-12 11:15:13 +0200

    laravel installed, added migrations

---
+++ database/migrations/2023_09_28_133031_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_producer')->constrained('producers');
+            $table->foreignId('id_category')->constrained('categories');
+            $table->foreignId('id_subcategory')->constrained('subcategories');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
